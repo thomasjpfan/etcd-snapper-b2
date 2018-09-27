8d4b1c5410676a3cbf36a6961fceee6511497ea5
Thomas Fan <thomasjpfan@gmail.com>
2018-09-27 16:35:04 -0400

    ENH: Changes tag formatting

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker_hub_name="${DOCKER_USERNAME}/etcd-snapper-b2"
-release_tag=$(date -u "+%Y%m%dT%H%M%S")
+release_tag=$(date -u "+%Y-%m-%dT%H-%M-%S")
 
 master_image="${docker_hub_name}:master"
 latest_image="${docker_hub_name}:latest"
